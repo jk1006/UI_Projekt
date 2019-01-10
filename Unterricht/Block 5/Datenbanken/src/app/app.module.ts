ab4f87531ea33c60513dda7b0710eb2f0ec57b76
Eric Werner <gruschtelapps@gmail.com>
2019-01-10 15:13:31 +0100

    Unterricht/Block5/DB
    Subjects Klassen hinzugefügt

--- Unterricht/Block 5/Datenbanken/src/app/app.module.ts
+++ Unterricht/Block 5/Datenbanken/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubjectsComponent } from './subjects/subjects.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubjectsComponent
   ],
   imports: [
     BrowserModule,
---
+++ Unterricht/Block 5/Datenbanken/src/app/subject.ts
@@ -0,0 +1,10 @@
+export class Subject {
+    id: number;
+    name: string;
+    description: string;
+    day: string;
+    startTime: string;
+    endTime: string;
+    room: String;
+    isApplied: boolean;
+}
