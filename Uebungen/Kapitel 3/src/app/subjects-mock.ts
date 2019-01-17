0be6279cc15d7c87c1aa8227374a012a34e3db2b
jk1006 <julius.klotz92@gmail.com>
2019-01-17 19:37:35 +0100

    Added DB connection using XMLHTTP requests^

--- Uebungen/Kapitel 3/src/app/subjects-mock.ts
+++
@@ -1,9 +0,0 @@
-import { Subject } from './subject';
-
-export const SUBJECTS: Subject[] = [
-    { id: 1, name: 'Grundlagen der Informatik', startTime: '08:00', endTime: '09:30', room: 'A2.03' },
-    { id: 2, name: 'Mathematik I', startTime: '08:00', endTime: '11:15', room: 'A2.07' },
-    { id: 3, name: 'Software Engineering', startTime: '14:00', endTime: '17:15', room: 'A2.03' },
-    { id: 4, name: 'Grundlagen der Informatik', startTime: '08:00', endTime: '11:15', room: 'A2.05' }
-];
-
